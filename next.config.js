ea25b82f2245f5405c56a0120ee8cd0b6740cf3d
N.Guy <32185091+ngabonzizaguy@users.noreply.github.com>
2025-03-28 13:24:45 +0200

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add other Next.js configs as needed
+};
+
+module.exports = nextConfig;
